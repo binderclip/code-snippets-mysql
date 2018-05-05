c6473c46dd16f0f01a1d5bfa1c83f454a35bdc81
clip <binderclip2014@gmail.com>
2018-05-05 13:33:04 +0800

    update where

--- table/create_table/create_table.sql
+++ table/create_table/create_table.sql
@@ -11,6 +11,8 @@ CREATE TABLE foo2
     PRIMARY KEY (`id`),
     UNIQUE KEY `uk_k` (`k`)
 );
+INSERT INTO foo2 (id, k, v)
+    VALUES (4, 2, 1), (5, 3, 2), (7, 4, 4), (8, 1, 1);
 
 CREATE TABLE foo3
 (
@@ -22,3 +24,13 @@ CREATE TABLE foo3
 );
 INSERT INTO foo3 (a, b, c)
     VALUES (1, 1, 1), (1, 1, 2), (1, 2, 1), (1, 2, 2);
+
+CREATE TABLE foo4
+(
+    id INT(10) UNSIGNED NOT NULL auto_increment,
+    d INT(10) UNSIGNED,
+    e INT(10) UNSIGNED,
+    PRIMARY KEY (`id`)
+);
+INSERT INTO foo4 (d, e)
+    VALUES (1, 11), (2, NULL), (3, 33), (4, NULL);
