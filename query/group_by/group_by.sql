871447086115c7136544b6da857e2a6ecd745da0
clip <binderclip2014@gmail.com>
2018-05-06 16:16:11 +0800

    update group_by

--- query/group_by/group_by.sql
+++ query/group_by/group_by.sql
@@ -1,5 +1,12 @@
-SELECT 
+SELECT
     COUNT(*), foo.bar
 FROM
     foo
 GROUP BY bar;
+
+SELECT * FROM foo3 GROUP BY b;  -- 只显示某个 group 的第一条数据
+SELECT * FROM foo3 GROUP BY c;
+SELECT id, b, COUNT(*) FROM foo3 GROUP BY b;
+SELECT id, b, COUNT(*) FROM foo3 GROUP BY b ORDER BY b DESC;
+
+SELECT b, c, COUNT(*) FROM foo3 GROUP BY b, c;
