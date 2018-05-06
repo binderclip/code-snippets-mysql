22dd6d2928f6d26b96a0694a68a600a1b60da5b4
clip <binderclip2014@gmail.com>
2018-05-06 22:42:03 +0800

    add inner join

---
+++ query/join/inner_join.sql
@@ -0,0 +1,12 @@
+SELECT vend_name, prod_id, prod_price FROM Vendors, Products WHERE Vendors.vend_id=Products.vend_id;
+
+SELECT vend_name, prod_id, prod_price FROM Vendors, Products;    -- 叉联结 cross join
+
+SELECT vend_name, prod_id, prod_price FROM Vendors INNER JOIN Products ON Vendors.vend_id=Products.vend_id;
+
+SELECT prod_name, vend_name, prod_price, quantity FROM OrderItems, Products, Vendors WHERE Vendors.vend_id=Products.vend_id AND Products.prod_id=OrderItems.prod_id AND order_num=20007;
+
+SELECT prod_name, vend_name, prod_price, quantity FROM OrderItems
+    INNER JOIN Products ON OrderItems.prod_id=Products.prod_id
+    INNER JOIN Vendors ON Products.vend_id=Vendors.vend_id
+    WHERE order_num=20007;
---
+++ query/join/readme.md
@@ -0,0 +1,5 @@
+# MySQL Join
+
+## refs
+
+- [mysql - Multiple inner joins with multiple tables - Stack Overflow](https://stackoverflow.com/questions/7150088/multiple-inner-joins-with-multiple-tables)
