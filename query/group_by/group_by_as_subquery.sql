667d3790c4ced38ae50a296fd4226d8d662b9968
clip <binderclip2014@gmail.com>
2018-05-08 11:03:47 +0800

    update group by

--- query/group_by/group_by_as_subquery.sql
+++ query/group_by/group_by_as_subquery.sql
@@ -11,4 +11,8 @@ WHERE
 ORDER BY bar_count DESC;
 -- https://stackoverflow.com/a/27619065/3936457
 
+-- 分组取数量、过滤数量、排序
 SELECT COUNT(*) AS bar_count, bar FROM foo GROUP BY bar HAVING bar_count > 1 ORDER BY bar_count DESC;
+-- 分组取数量、过滤数量、再取总数
+SELECT COUNT(*) FROM
+	(SELECT COUNT(*) AS bar_count, bar FROM foo GROUP BY bar HAVING bar_count > 1 ORDER BY bar_count) AS s;
