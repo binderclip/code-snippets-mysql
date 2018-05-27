34dc3129ecc8efc8492c2c3887da74cf38dbca2e
clip <binderclip2014@gmail.com>
2018-05-27 18:59:29 +0800

    update

--- query/update/update.sql
+++ query/update/update.sql
@@ -2,4 +2,20 @@ UPDATE foo SET bar = bar + 1;
 
 UPDATE foo SET bar = bar - 1 WHERE bar > 1;
 
-UPDATE recipe_feed SET read=0 WHERE user_id=105697109 AND author_id=105697106 AND recipe_id=1000000382;
+UPDATE recipe_feed SET
+    read=0
+WHERE
+    user_id=55555 AND author_id=55555 AND recipe_id=1111111;
+
+UPDATE Customers SET cust_email = 'kim@thetoystore.com' WHERE cust_id = '1000000005';
+
+UPDATE Customers SET
+	cust_contact = 'Sam Roberts',
+	cust_email = 'sam@toyland.com'
+WHERE
+	cust_id = '1000000006';
+
+UPDATE Customers
+	SET cust_email = NULL
+WHERE
+	cust_id = '1000000005';
