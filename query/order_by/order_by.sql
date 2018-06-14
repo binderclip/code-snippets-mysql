5e564947efa7c20c74668b5834faebda88f22b48
clip <binderclip2014@gmail.com>
2018-06-14 11:44:39 +0800

    update

--- query/order_by/order_by.sql
+++ query/order_by/order_by.sql
@@ -7,3 +7,8 @@ SELECT * FROM foo3 ORDER BY b DESC, c;
 SELECT * FROM foo3 ORDER BY b DESC, c DESC;
 
 SELECT id, a, b FROM foo3 ORDER BY c, 3 DESC;
+
+-- 优先按 order_item 排，order_item 相同的时候按照 order_num
+SELECT order_num, order_item FROM OrderItems ORDER BY order_item, order_num;
+-- 与上一条相反
+SELECT order_num, order_item FROM OrderItems ORDER BY order_num, order_item;
