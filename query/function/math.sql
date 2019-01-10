d817ebca4183f20e88c5579cf1416974f48d3e51
clip <binderclip2014@gmail.com>
2019-01-10 15:55:55 +0800

    time and math

---
+++ query/function/math.sql
@@ -0,0 +1,5 @@
+SELECT TRUNCATE(1.999,1);
+-- 1.9
+SELECT TRUNCATE(1.999,0);
+-- 1
+-- https://dev.mysql.com/doc/refman/8.0/en/mathematical-functions.html#function_truncate
--- query/function/time.sql
+++ query/function/time.sql
@@ -2,3 +2,5 @@ select weekofyear('2015-12-31'); -- 53
 select weekofyear('2018-12-31'); -- 1
 select weekofyear('2018-12-30'); -- 52
 select weekofyear('2018-12-29'); -- 52
+
+select unix_timestamp('2018-12-29'); -- 1546041600
