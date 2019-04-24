a2e52ea0872d9d139104211265a774df3129c908
clip <binderclip2014@gmail.com>
2019-04-24 15:13:10 +0800

    count and case

--- query/_method/count.sql
+++ query/_method/count.sql
@@ -12,3 +12,18 @@ SELECT 1 FROM foo4;
 SELECT COUNT(DISTINCT e) FROM foo4;     -- 不计算 NULL
 SELECT COUNT(DISTINCT b) FROM foo3;     -- 只计算不一样的数量
 SELECT COUNT(b) FROM foo3;
+
+
+-- count and case
+create table mytable (
+   id int(10) unsigned not null auto_increment,
+  primary key (id)
+);
+insert into mytable (
+  id
+) values
+(1), (2), (3), (4), (5);
+select
+  count(*),
+  count(case when id > 3 then 1 end) cnt_id_bigger_than_avg
+from mytable;
