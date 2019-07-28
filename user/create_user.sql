ce2e327f93210c99f9ba14d67c07b04ecf24cd0d
clip <binderclip2014@gmail.com>
2019-07-28 10:08:42 +0800

    create user
    Change-Id: I0eff7919871689863ff54d9e9accad2cfeb38ee9

---
+++ user/create_user.sql
@@ -0,0 +1,6 @@
+CREATE USER 'newuser'@'localhost' IDENTIFIED BY 'user_password';
+CREATE USER 'newuser'@'10.8.0.5' IDENTIFIED BY 'user_password';
+CREATE USER 'newuser'@'%' IDENTIFIED BY 'user_password';
+
+-- How to Create MySQL Users Accounts and Grant Privileges | Linuxize
+-- https://linuxize.com/post/how-to-create-mysql-user-accounts-and-grant-privileges/
